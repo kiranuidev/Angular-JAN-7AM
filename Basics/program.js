95fa15a2151c961ffd733f0fb68fa609b005bbfa
narasimha kbnd <narasimha kbnd>
2017-01-24 08:09:41 +0530

    added basics

---
+++ Basics/program.js
@@ -0,0 +1,18 @@
+/*var welcome ="Welcome to debugging session";
+
+console.log(welcome);*/
+
+//calculating total of grocries.
+
+var groceries = ["Milk","Dal","Rice"];
+console.log(groceries[0]);
+console.log(groceries[1]);
+console.log(groceries[2]);
+
+console.log(groceries.length);
+//Adding a property to collection.
+groceries[3]="Honey";
+console.log(groceries.length);
+for(var i=0;i<groceries.length;i++){
+  //do something.   
+}
