fc3ea3492b9f0a08ee595d04c15a8eb2ba7f4356
narasimha kbnd <narasimha kbnd>
2017-02-07 09:05:05 +0530

    added code

--- ObjectOriented/constructors.js
+++ ObjectOriented/constructors.js
@@ -12,6 +12,11 @@ var car = new vehicle();
 car.engineCapacity="1200cc"
 car.seatingCapacity=2;
 
+var cruise = new vehicle();
+cruise.noOfSeats=100;
+cruise.engineCapacity="4000cc";
+
+
 var bmw= new Object(car);
 console.log(bmw.engineCapacity);
 console.log(bmw.seatingCapacity);
--- ObjectOriented/employee.js
+++ ObjectOriented/employee.js
@@ -2,6 +2,8 @@ function Person(panData) {
     this.firstName = "";
     this.lastName = "";
     var panNumber = panData;
+    this.age=""
+    this.gender="";
     this.maskedPAN = function () {
         var panLastFourDigits = panNumber.substring(6, 10);
         return "XXX-XXX-"+panLastFourDigits;
@@ -10,3 +12,14 @@ function Person(panData) {
 
 var employee = new Person("123AGK9870");
 console.log(employee.maskedPAN());
+
+var manager = new Person("1837749494");
+manager.reporteesCount =10;
+-----------------------------------------------
+    class Person{
+        
+    }
+    class manager extends Person{
+      public reporteesCount {get;set;}  
+    }
+
